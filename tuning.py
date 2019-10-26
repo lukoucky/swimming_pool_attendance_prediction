33e92f82a0b59104e4e00b143b343027a050d792
lukasus <lukoucky@gmail.com>
2019-10-26 19:41:04 +0200

    Update on tuning

--- tuning.py
+++ tuning.py
@@ -1,27 +1,26 @@
 from models.random_forest_classifier import RandomForest
 from utils import MyGridSearch
-from utils import Helper
 from sklearn.ensemble import RandomForestClassifier
 
-# rf = RandomForest()
-# # rf.tune_parameters_on_full_training_data()
-# rf.load_tuned_parameters()
-# rf.set_best_tuned_parameters()
-# rf.evaluate_model_on_testing_data()
-# # rf.show_n_days_prediction(1)
-# rf.get_testing_mse()
+def tune_random_forest():
+	max_depth = [10, 50] 
+	max_features = [2, 5, 10, 20] 
+	min_samples_leaf = [1, 2, 4]
+	min_samples_split = [2, 5] 
+	n_estimators = [10, 20] 
+	random_grid = {	'n_estimators': n_estimators,
+					'max_features': max_features,
+					'max_depth': max_depth,
+					'min_samples_split': min_samples_split,
+					'min_samples_leaf': min_samples_leaf}
 
-bootstrap = [True, False]
-max_depth = [10, 50, 100, None] 
-max_features = [20, 50, None] 
-min_samples_leaf = [1, 2, 4]
-min_samples_split = [2, 5, 10] 
-n_estimators = [10, 20] 
-random_grid = {	'n_estimators': n_estimators,
-				'max_features': max_features,
-				'max_depth': max_depth,
-				'min_samples_split': min_samples_split,
-				'min_samples_leaf': min_samples_leaf}
+	mgs = MyGridSearch('RandomForestClassifier',random_grid)
+	mgs.fit(True)
 
-mgs = MyGridSearch('RandomForestClassifier',random_grid)
-mgs.fit()
+if __name__ == '__main__':
+	rf = RandomForest()
+	rf.without_reserved = True
+	rf.fit()
+	rf.save_model('data/rfc_nores.pickle')
+	rf.show_n_days_prediction(12)
+	print(rf.get_testing_mse())
