a1be77f5cb207ca4677a015aed774fea28f2fe88
lukasus <lukoucky@gmail.com>
2019-11-02 20:00:08 +0100

    Remove days statistics from utils and move it to separate file

---
+++ days_statistics.py
@@ -0,0 +1,124 @@
+import copy
+import pickle
+import os
+import matplotlib.pyplot as plt
+
+
+class DaysStatistics:
+	def __init__(self):
+		self.days = list()
+		self.averages_weekday = list()
+		self.averages_weekend = list()
+		self.generate_averages()
+
+	def generate_averages(self, pickle_path='data/days_statistics.pickle', override_pickle=False):
+		if os.path.isfile(pickle_path) and not override_pickle:
+			with open(pickle_path, 'rb') as input_file:
+				self.averages_weekday, self.averages_weekend = pickle.load(input_file)
+		else:
+			n_weekday = list()
+			sums_weekday = list()
+			n_weekend = list()
+			sums_weekend = list()
+			for month in range(12):
+				self.averages_weekday.append([])
+				self.averages_weekend.append([])
+				n_weekday.append([])
+				sums_weekday.append([])
+				n_weekend.append([])
+				sums_weekend.append([])
+				for i in range(288):
+					self.averages_weekday[month].append(0)
+					self.averages_weekend[month].append(0)
+					n_weekday[month].append(0)
+					sums_weekday[month].append(0)
+					n_weekend[month].append(0)
+					sums_weekend[month].append(0)
+
+			for day in self.days:
+				for index, row in day.data.iterrows():
+					month = row['month']-1
+					day_id = self.get_list_id(row['hour'], row['minute'])
+					if row['day_of_week'] < 5:
+						sums_weekday[month][day_id] += int(row['pool'])
+						n_weekday[month][day_id] += 1
+					else:
+						sums_weekend[month][day_id] += int(row['pool'])
+						n_weekend[month][day_id] += 1
+
+			for month in range(12):
+				for i in range(288):
+					if n_weekday[month][i] > 0:
+						self.averages_weekday[month][i] = sums_weekday[month][i]/n_weekday[month][i]
+					if n_weekend[month][i] > 0:
+						self.averages_weekend[month][i] = sums_weekend[month][i]/n_weekend[month][i]
+
+			with open(pickle_path, 'wb') as f:
+				pickle.dump([self.averages_weekday, self.averages_weekend], f)
+
+	def get_average_for_month(self, month, weekend):
+		if weekend:
+			return self.averages_weekend[month]
+		else:
+			return self.averages_weekday[month]
+
+	def get_average_for_month_at_time(self, month, hour, minute, weekend):
+		if weekend:
+			return self.averages_weekend[month][self.get_list_id(hour, minute)]
+		else:
+			return self.averages_weekday[month][self.get_list_id(hour, minute)]
+
+	def get_month_average_for_time(self, month, hour, minute):
+		day_id = self.get_list_id(hour, minute)
+		return self.averages_weekday[month][day_id]
+
+	def get_average_for_last_days(self, n_days, month, day, hour, minute):
+		pass 
+
+	def get_average_for_last_days_at_time(self, n_days, month, day, hour, minute):
+		pass 
+
+	def get_list_id(self, hour, minute):
+		return hour*12 + minute//5
+
+	def plot_year_averages_by_month(self, weekend):
+		plot_name = 'Average monthly attandance for '
+		if weekend:
+			plot_name += 'weekends'
+		else:
+			plot_name += 'weekdays'
+
+		fig = plt.figure(plot_name, figsize=(19,12))
+		for i in range(12):
+			if weekend:
+				data = self.averages_weekend[i]
+			else:
+				data = self.averages_weekday[i]
+
+			ax = fig.add_subplot(4, 3, i+1)
+			ax.title.set_text('Month %d' % (i+1))
+			l1, = plt.plot(data)
+			ax.grid(True)
+			ax.set_ylim([0,300])
+			ax.set_xlim([0,300])
+		plt.show()
+
+	def plot_monthly_average(self, month, weekend, other_data=None, other_data_offset=48):
+		plot_name = 'Average attandance for '
+		if weekend:
+			plot_name += 'weekends'
+			data = self.averages_weekend[month]
+		else:
+			plot_name += 'weekdays'
+			data = self.averages_weekday[month]
+
+		fig = plt.figure(plot_name + 'month ' + str(month))	
+		ax = fig.add_subplot(1,1,1)
+		l1, = plt.plot(data)
+		if other_data is not None:
+			y = range(other_data_offset,other_data_offset+len(other_data))
+			l2, = plt.plot(y, other_data)
+
+		ax.grid(True)
+		ax.set_ylim([0,300])
+		plt.show()
