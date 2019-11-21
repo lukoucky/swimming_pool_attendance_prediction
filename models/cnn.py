947020e65486836cfa365d45968d648f2e2f6b5f
lukasus <lukoucky@gmail.com>
2019-11-21 20:36:54 +0100

    CNN using new base model
    Former-commit-id: 66d4313056c4dd8b9e877e2e6e7d00a656c76190

--- models/cnn.py
+++ models/cnn.py
@@ -1,26 +1,28 @@
-from data_helper import DataHelper
-import numpy as np
-import pickle
-import os
+from models.neural_network_base import NeuralNetworkBase
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import Flatten
 from keras.layers.convolutional import Conv1D
 from keras.layers.convolutional import MaxPooling1D
-from keras.models import model_from_json
 
-class ConvolutionalNeuralNetwork():
+
+class ConvolutionalNeuralNetwork(NeuralNetworkBase):
+	"""
+	Convolutional Neural Network model. Using 1D convolutional layers at input.
+	"""
 	def __init__(self, model_name=None):
-		self.dh = DataHelper()
-		self.columns = ['pool','day_of_week','month','minute_of_day','year'] 
-		self.time_steps_back = 3
-		self.model = None
-		self.model_name = model_name
-		if model_name is None:
-			self.model_name = 'cnn_model'
+		"""
+		Constructor initializies base constructor and sets default model_name if not given in input argument
+		"""
+		super(ConvolutionalNeuralNetwork, self).__init__(model_name)
+		if self.model_name is None:
+			self.model_name = 'cnn_model'		
 		self.build_model()
 
 	def build_model(self):
+		"""
+		Builds and compiles CNN model.
+		"""
 		n_features = len(self.columns)
 		self.model = Sequential()
 		self.model.add(Conv1D(filters=64, kernel_size=2, activation='relu', input_shape=(self.time_steps_back, n_features)))
@@ -29,32 +31,3 @@ class ConvolutionalNeuralNetwork():
 		self.model.add(Dense(100, activation='relu'))
 		self.model.add(Dense(1))
 		self.model.compile(optimizer='adam', loss='mse')
-
-	def fit_with_training_data(self, epochs=10):
-		x_train, y_train, x_test, y_test = self.dh.generate_feature_vectors_for_cnn(self.columns, self.time_steps_back)
-		self.fit(x_train, y_train, epochs)
-
-	def fit(self, x_train, y_train, epochs=10):
-		if self.model is not None:
-			self.model.fit(x_train, y_train, epochs=epochs, validation_split=0.3)
-			self.save_model()
-		else:
-			print('Cannot fit. Build model first.')
-
-	def predict(self, x):
-		return self.model.predict(x)
-
-	def save_model(self):
-		model_json = self.model.to_json()
-		with open(self.model_name+'.json', 'w') as json_file:
-		    json_file.write(model_json)
-
-		self.model.save_weights(self.model_name+'_weights.h5')
-		print('CNN model saved to disk with name' + self.model_name)
-
-	def load_model(self):
-		json_file = open(self.model_name+'.json', 'r')
-		model_json = json_file.read()
-		json_file.close()
-		self.model = model_from_json(model_json)
-		self.model.load_weights(self.model_name+'_weights.h5')
