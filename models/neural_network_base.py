268682a658d74a2da813b6c571b0b9a1b7e07fba
lukasus <lukoucky@gmail.com>
2019-11-23 11:31:22 +0100

    Add fit history and MSE testing to NN base class

--- models/neural_network_base.py
+++ models/neural_network_base.py
@@ -20,7 +20,7 @@ class NeuralNetworkBase(ABC):
 		self.time_steps_back = 5
 		self.model = None
 		self.model_name = model_name
-		self.fit_history = dict()
+		self.fit_history = {'val_loss':list(), 'loss':list(), 'mse':list()}
 
 	@abstractmethod
 	def build_model(self):
@@ -30,18 +30,19 @@ class NeuralNetworkBase(ABC):
 		"""
 		pass
 
-	def fit_with_training_data(self, epochs=10, validation_split=0.3, verbose=1, use_cpu=True):
+	def fit_with_training_data(self, epochs=10, validation_split=0.3, verbose=1, use_cpu=True, test_mse=True):
 		"""
 		Trains model for defined number of epochs on training data.
 		:param epochs: Number of epochs for fitting.
 		:param validation_split: Percentage of training data that will be used for validation
 		:param verbose: Verbose settign of fit function
 		:param use_cpu: If True it will force to use CPU even if GPU is available
+		:param test_mse: If True mean square error on testing set will be tested after fitting is done and result save in history
 		"""
 		x_train, y_train, x_test, y_test = self.dh.generate_feature_vectors_for_cnn(self.columns, self.time_steps_back)
-		self.fit(x_train, y_train, epochs, validation_split, verbose, use_cpu)
+		self.fit(x_train, y_train, epochs, validation_split, verbose, use_cpu, test_mse)
 
-	def fit(self, x_train, y_train, epochs=10, validation_split=0.3, verbose=1, use_cpu=True):
+	def fit(self, x_train, y_train, epochs=10, validation_split=0.3, verbose=1, use_cpu=True, test_mse=True):
 		"""
 		Trains model for defined number of epochs on provided data.
 		:param x_train: Numpy array with training data
@@ -50,13 +51,15 @@ class NeuralNetworkBase(ABC):
 		:param validation_split: Percentage of training data that will be used for validation
 		:param verbose: Verbose settign of fit function
 		:param use_cpu: If True it will force to use CPU even if GPU is available
+		:param test_mse: If True mean square error on testing set will be tested after fitting is done and result save in history
 		"""
 		if self.model is not None:
 			if use_cpu:
 				with tf.device('/cpu:0'):
-					self.fit_history = self.model.fit(x_train, y_train, epochs=epochs, validation_split=validation_split, verbose=verbose)
+					history = self.model.fit(x_train, y_train, epochs=epochs, validation_split=validation_split, verbose=verbose)
 			else:
-				self.fit_history = self.model.fit(x_train, y_train, epochs=epochs, validation_split=validation_split, verbose=verbose)
+				history = self.model.fit(x_train, y_train, epochs=epochs, validation_split=validation_split, verbose=verbose)
+			self.update_fit_history(history, test_mse)
 			self.save_model()
 		else:
 			print('Cannot fit. Build model first.')
@@ -69,27 +72,36 @@ class NeuralNetworkBase(ABC):
 		"""
 		return self.model.predict(x)
 
-	def save_model(self):
+	def save_model(self, name_addition=None):
 		"""
 		Saves model and weights with name defined in model_name
 		"""
+		name = self.model_name
+		if name_addition is not None:
+			name += name_addition
+
 		model_json = self.model.to_json()
-		with open(self.model_name+'.json', 'w') as json_file:
+		with open(name+'.json', 'w') as json_file:
 		    json_file.write(model_json)
 
-		self.model.save_weights(self.model_name+'_weights.h5')
-		print('Model saved to disk with name:' + self.model_name)
+		self.model.save_weights(name+'_weights.h5')
+		print('Model saved to disk with name: ' + name)
 
-	def load_model(self):
+	def load_model(self, name_addition=None):
 		"""
 		Loads model and weights with name defined in model_name
+		:param name_addition: 
 		"""
-		json_file = open(self.model_name+'.json', 'r')
+		name = self.model_name
+		if name_addition is not None:
+			name += name_addition
+
+		json_file = open(name+'.json', 'r')
 		model_json = json_file.read()
 		json_file.close()
 		self.model = model_from_json(model_json)
-		self.model.load_weights(self.model_name+'_weights.h5')
-		print('Model %s loaded from disk'%(self.model_name))
+		self.model.load_weights(name+'_weights.h5')
+		print('Model %s loaded from disk'%(name))
 
 	def setup(self, columns, time_steps_back):
 		"""
@@ -121,4 +133,27 @@ class NeuralNetworkBase(ABC):
 		"""
 		Prints mean square error
 		"""
-		print('MSE = %.2f'%(self.get_mse()))
+		print('\nMSE = %.2f\n'%(self.get_mse()))
+
+	def update_fit_history(self, history, test_mse):
+		"""
+		Adds progress of validation loss and training loss to `fit_history`
+		If test_mse is True than also tests MSE and adds result to `fit_history`
+		:param history: Keras fit history object
+		:param test_mse: Flag if MSE should be tested
+		"""
+		if 'val_loss' in history.history.keys():
+			for value in history.history['val_loss']:
+				self.fit_history['val_loss'].append(value)
+		if 'loss' in history.history.keys():
+			for value in history.history['loss']:
+				self.fit_history['loss'].append(value)
+		if test_mse:
+			mse = self.get_mse()
+			if len(self.fit_history['mse']) > 0 and mse < min(self.fit_history['mse']):
+				name_addition = '_MSE_%.0f'%(mse)
+				self.save_model(name_addition)
+				print('\nNew best MSE = %.2f\n'%(mse))
+			else:
+				print('\nMSE = %.2f\n'%(mse))
+			self.fit_history['mse'].append(mse)			
