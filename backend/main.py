c298456fb4b77e385ba702f097925a551fd91a6e
lukasus <lukoucky@gmail.com>
2020-01-17 11:16:14 +0100

    Update flask server to return predictions correctly and add reserved lines into attendance.

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,7 @@
 import json
 import pandas as pd
 from functools import wraps
-from flask import Flask, send_from_directory, redirect, request, current_app
+from flask import Flask, jsonify, send_from_directory, redirect, request, current_app
 app = Flask(__name__)
 
 def support_jsonp(f):
@@ -16,36 +16,34 @@ def support_jsonp(f):
 			return f(*args, **kwargs)
 	return decorated_function
 
-
-@app.route('/')
-def index():
-	return '<h1>Hello World!</h1>'
-
-
-@app.route('/attendance/<filename>')
-def data_regular(filename):
-	print(filename)
-	return send_from_directory('/var/www/html/data', filename)
-
-
-@app.route('/prediction/<filename>')
-def data_tree(filename):
-	print(filename)
-	return send_from_directory('/var/www/html/data/prediction_extra_tree', filename)
-
-
-@default.route('/test/<filename>', methods=['GET'])
+@app.route('/attendance/<filename>', methods=['GET'])
 @support_jsonp
-def test(filename):
-	df = pd.read_csv('/var/www/html/data/prediction_extra_tree/'+filename)
-	prediction = ''
-	for i, row in df.iterrows():
-	    prediction += str(row['pool'])+','
-	return jsonify({"prediction":prediction[:-1]})
+def get_attendace(filename):
+	filepath = '/var/www/html/data/'+filename
+	attendance = get_data(filepath,'pool')
+	lines = get_data(filepath,'lines_reserved')
+	return jsonify({'attendance':attendance, 'lines_reserved':lines})
 
+@app.route('/prediction/extra_trees/<filename>', methods=['GET'])
+@support_jsonp
+def get_extra_trees_prediction(filename):
+	filepath = '/var/www/html/data/prediction_extra_tree/'+filename
+	prediction = get_data(filepath,'pool')
+	return jsonify({'prediction':prediction})
+
+@app.route('/prediction/average/<filename>', methods=['GET'])
+@support_jsonp
+def get_monthly_average_prediction(filename):
+	filepath = '/var/www/html/data/prediction_monthly_average/'+filename
+	prediction = get_data(filepath,'pool')
+	return jsonify({'prediction':prediction})
+
+def get_data(filepath, column):
+	df = pd.read_csv(filepath)
+	values = ''
+	for i, row in df.iterrows():
+		values += str(row[column])+','
+	return values[:-1]
 
 if __name__ == '__main__':
 	app.run(debug=True, host='0.0.0.0')
-
-
-
