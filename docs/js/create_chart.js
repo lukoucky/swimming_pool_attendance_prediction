da86b04b21e5290edf84ce33a902f0c2ad242310
Lukas Koucky <lukoucky@gmail.com>
2020-01-21 23:05:11 +0100

    Add mean squared error of all algorithms for selected day to web.

--- docs/js/create_chart.js
+++ docs/js/create_chart.js
@@ -49,7 +49,15 @@ var config = {
     }
 };
 
+const algorithm = {
+    AVG: 'Monthly Average',
+    EXTRA: 'Extra Trees Regressor',
+    LSTM: 'LSTM'
+}
+
 const server_address = 'https://lukoucky.com:5000'
+
+var real_attendance = [];
 
 var config_copy = JSON.parse(JSON.stringify(config));
 var today_plus_week = new Date();
@@ -74,6 +82,7 @@ var picker = new Pikaday(
 $( "#datepicker" ).change(function() {
   console.log("datepicker change to "+document.getElementById('datepicker').value);
   var today = picker.toString('YYYY-MM-DD');
+  document.getElementById('mse').innerHTML = '';
   resetCanvas();
   updateChart(today);
 });
@@ -98,16 +107,30 @@ function updateChart(date_string){
         success: function(response)  
         {
             generateChart(response, config, date_string);
-            addDataFromCSV(server_address+'/prediction/average/'+day_api_string, 'rgba(67, 175, 105,0.9)', 'Monthly Average');
-            addDataFromCSV(server_address+'/prediction/extra_trees/'+day_api_string, 'rgba(102, 46, 155,0.9)', 'Extra Trees Regressor');
+            real_attendance = response.attendance.split(',');
+
+            addDataFromCSV(server_address+'/prediction/average/'+day_api_string, 'rgba(67, 175, 105,0.9)', algorithm.AVG);
+            addDataFromCSV(server_address+'/prediction/extra_trees/'+day_api_string, 'rgba(102, 46, 155,0.9)', algorithm.EXTRA);
             // addDataFromCSV('data/prediction_random_forest/'+date_string+'.csv', 'rgba(248, 102, 36,0.9)', 'Random Forest Regressor');
             // addDataFromCSV('data/test/prediction_algo2/2019-11-02.csv', 'rgba(67, 175, 105,0.9)', 'Hidden Markov Model');
             // addDataFromCSV('data/test/prediction_algo2/2019-11-02.csv', 'rgba(14,124,123,0.9)', 'Long Short Term Memory');
         }   
-    });  
-
+    });
     config.options.scales.xAxes[0].ticks.min = '6:00';
     config.options.scales.xAxes[0].ticks.max = '22:00';
+}
+
+function compute_mse(algorithm, prediction){
+    total = 0;
+    n = 0;
+    for(var i = 72; i < 264; i++){
+        if(!isNaN(real_attendance[i]) && !isNaN(prediction[i]))
+        {
+            total += Math.pow(Number(real_attendance[i]) - Number(prediction[i]), 2);
+            n += 1;
+        }
+    }
+    document.getElementById('mse').innerHTML += '<li>'+algorithm+': '+Number(total/n).toFixed(0)+'</li>';
 }
 
 function resetCanvas(){
@@ -128,21 +151,23 @@ function generateChart(data, conf, date_string){
     window.myLine = new Chart(ctx, conf);
 }
 
- function addDataFromCSV(data_path, color, title){
+ function addDataFromCSV(data_path, color, algorithm){
     $.ajax({
     type: "GET",  
     url: data_path,
     dataType: "jsonp",       
     success: function(response)  
     {
-        addData(window.myLine, response.prediction.split(','), "people", true, color, title);
+        prediction = response.prediction.split(',')
+        addData(window.myLine, prediction, "people", true, color, algorithm);
+        compute_mse(algorithm, prediction);
     }   
     });   
  }
 
  function addData(chart, data, type, updateNow, color, title) {
     color = color || "rgba(234,53,70,1)"
-    title = title || 'Actual occupancy'
+    title = title || 'Actual attendance'
     if(type == 'people'){
     chart.data.datasets.push({
             label: title,
